7847aed9fe1518246907382ef32d654f36d9b111
leeeeeexuus <anizikov@gmail.com>
2024-03-09 00:06:38 +0300

    Initial Commit

---
+++ Mob/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Mob
+//
+//  Created by Aleksey Nizikov on 02.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("man")
+            .aspectRatio(contentMode: .fit)
+    }
+}
+
+#Preview {
+    ContentView()
+}
